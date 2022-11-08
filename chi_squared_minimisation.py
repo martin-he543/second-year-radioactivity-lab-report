0ee1b9b10336518aa96ebc826d4891629d0c4452
Martin He <41821907+martin-he543@users.noreply.github.com>
2022-11-08 08:51:47 +0000

    formatting

--- chi_squared_minimisation.py
+++ chi_squared_minimisation.py
@@ -58,7 +58,6 @@ print("c₂ = ",poly19C[1],"+/-",np.sqrt(cov_poly19C[1][1]))
 area,unc_area = 1.97e-4, 1.4e-5
 def exponential_decay(d,A,mu,k,B):                                                                                 
      return A*area*np.exp(-mu*d+k)/(4*np.pi) + B
-
 params, cov_params = curve_fit(exponential_decay,x19,count19, p0=[555000,1.6,10,10])
 
 x=np.linspace(0,5.2,10000)
@@ -104,19 +103,15 @@ plt.title("Task 20: Range of Decay Particles through Cu", **titleFont)
 poly20A,cov_poly20A = np.polyfit(x20[:8],y20[:8],1,cov=True)
 x= np.linspace(0,0.8,1000)
 plt.plot(x,poly20A[0]*x + poly20A[1])
-
-
 poly20B,cov_poly20B = np.polyfit(x20[5:12],y20[5:12],1,cov=True)
 x= np.linspace(0.3,1.1,1000)
 plt.plot(x,poly20B[0]*x + poly20B[1],color="red")
-
 poly20C,cov_poly20C = np.polyfit(x20[14:],y20[14:],1,cov=True)
 x= np.linspace(0.8,1.6,1000)
 plt.plot(x,poly20C[0]*x + poly20C[1],color="green")
 
 x_intersection = -(poly20B[1] - poly20A[1])/(poly20B[0] - poly20A[0])
 print("first intersection",x_intersection,poly20A[0]*x_intersection+poly20A[1])
-
 x_intersection = -(poly20C[1] - poly20B[1])/(poly20C[0] - poly20B[0])
 print("second intersection",x_intersection,poly20B[0]*x_intersection+poly20B[1])
 
@@ -133,7 +128,6 @@ print("c₂ = ",poly20C[1],"+/-",np.sqrt(cov_poly20C[1][1]))
 area,unc_area = 1.97e-4, 1.4e-5
 def exponential_decay(d,A,mu,k,B):                                                                                 
      return A*area*np.exp(-mu*d+k)/(4*np.pi) + B
-
 params, cov_params = curve_fit(exponential_decay,x20,count20)
 
 x=np.linspace(0,1.55,10000)
